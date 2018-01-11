088c12179aba65d78493be4b46e7eac2c8995564
deur2008 <deur2010@hotmail.com>
2018-01-12 07:19:32 +0800

    done activity

--- Jobago2/app/src/main/java/com/example/vip/jobago/AdminView.java
+++ Jobago2/app/src/main/java/com/example/vip/jobago/AdminView.java
@@ -18,7 +18,22 @@ public class AdminView extends AppCompatActivity implements AdapterView.OnItemSe
         setContentView(R.layout.activity_admin_view);
 
 
+        //*******
+        //gender
+        //*******
+        Spinner spinner3 = (Spinner) findViewById(R.id.spinner4);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,R.array.gender_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
 
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner3.setAdapter(adapter3);
+
+        spinner3.setOnItemSelectedListener(this);
+
+        //*******
+        //education
+        //*******
         Spinner spinner = (Spinner) findViewById(R.id.spinner2);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.education_array, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
@@ -29,7 +44,33 @@ public class AdminView extends AppCompatActivity implements AdapterView.OnItemSe
 
         spinner.setOnItemSelectedListener(this);
 
+        //*******
+        //job
+        //*******
+        Spinner spinner1 = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,R.array.job_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner1.setAdapter(adapter1);
+
+        spinner1.setOnItemSelectedListener(this);
+
+        //*******
+        //city
+        //*******
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner3);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,R.array.city_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner2.setAdapter(adapter2);
+
+        spinner2.setOnItemSelectedListener(this);
     }
+
 
 
     @Override
